61d763a1fd53375a9633147be8edadaa812d4a22
Romez <Romez1990@gmail.com>
2020-07-11 09:24:52 +0300

    feat(ViewModels): create main view movel

---
+++ Numerology/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Numerology.Annotations;
+using Numerology.Models;
+
+namespace Numerology.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            var letterConverter = new LetterConverter();
+            _stringAdder = new StringAdder(letterConverter);
+        }
+
+        private readonly IStringAdder _stringAdder;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _inputString = "";
+
+        public string InputString
+        {
+            get => _inputString;
+            set
+            {
+                _inputString = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public string Result => _stringAdder.Add(InputString);
+    }
+}
